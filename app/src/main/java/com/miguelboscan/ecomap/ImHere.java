905c620dcf1ddc118fbdc83ad700f4c3e7af1bd0
rarivero12 <rafaelarturo16@gmail.com>
2015-06-12 02:08:21 -0430

    agregarevento

---
+++ app/src/main/java/com/miguelboscan/ecomap/ImHere.java
@@ -0,0 +1,12 @@
+package com.miguelboscan.ecomap;
+
+/**
+ * hi guys , I'm a programmer google trying to help your project work better, and they sent me a bug report from this address : GJRORE ; VKVVR ; KOROKRK0000000333 : # $$ #
+
+ I would appreciate confirmation and cooperation on his part
+
+ Antonie MicKals
+ */
+public class ImHere {
+
+}
